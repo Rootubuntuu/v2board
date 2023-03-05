aa0c652f83337f4904dcec9c14dc67ba58a42a8a
RootUbuntu <46374084+Rootubuntuu@users.noreply.github.com>
2023-03-06 03:02:10 +0330

    Update V2boardStatistics.php

--- app/Console/Commands/V2boardStatistics.php
+++ app/Console/Commands/V2boardStatistics.php
@@ -21,7 +21,7 @@ class V2boardStatistics extends Command
      *
      * @var string
      */
-    protected $description = '统计任务';
+    protected $description = 'Statistics task';
 
     /**
      * Create a new command instance.
